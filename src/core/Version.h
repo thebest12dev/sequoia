48d8f941eed906fb38410aff6544583f64098e7f
thebest12lines <userunamed981+github@gmail.com>
2025-12-02 20:54:10 +0600

    0.1.0 I mean

---
+++ src/core/Version.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Basically some stuff releated to versioning,
+
+#define SEQUOIA_VERSION 0x00000001 // Version code
+#define SEQUOIA_VERSION_MAJOR 0 // major
+#define SEQUOIA_VERSION_MAJOR 1 // minor
+#define SEQUOIA_VERSION_MAJOR 0 // patch
+#define SEQUOIA_VERSION_CHANNEL "alpha" // channel
